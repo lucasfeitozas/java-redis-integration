da943a9e3cd71f6a0df23abe247ababe7308714f
Lucas Feitozas <lucasfeitozas@gmail.com>
2024-01-22 19:29:43 -0400

    first commit

---
+++ src/main/java/br/com/lucasdev/javaredisintegration/JavaRedisIntegrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasdev.javaredisintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRedisIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRedisIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasdev/javaredisintegration/JavaRedisIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasdev.javaredisintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRedisIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
